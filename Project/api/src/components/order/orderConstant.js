db739a3af4b6dec542b4b69fa34947741348db19
thangyt01 <thangyt00@gmail.com>
2022-06-21 15:44:01 +0700

    update

---
+++ Project/api/src/components/order/orderConstant.js
@@ -0,0 +1,34 @@
+function orderResponseFormat(e){
+    return {
+        id: e.id,
+        user: {
+            user_id: e.user_id,
+            firstname: e.firstname,
+            lastname: e.lastname,
+            phone: e.phone,
+            address: e.address
+        },
+        order: {
+            order_id: e.order_id,
+            detail: [
+                {
+                    product_id: e.product_id,
+                    name: e.name,
+                    color: e.color,
+                    quantity: e.quantity,
+                    total_cost: e.total_cost
+                }
+            ],
+            total_cost: e.total_cost
+        },
+        date: e.date,
+        month: e.month,
+        year: e.year,
+        createdAt: e.createdAt,
+        updatedAt: e.updatedAt
+    }
+}
+
+module.exports = {
+    orderResponseFormat
+}
