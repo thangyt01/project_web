f1002b5ee399828c0cf63bb65d5e17e1a9a39107
thangyt01 <thangyt00@gmail.com>
2022-06-28 09:27:11 +0700

    update

--- Project/api/database/index.js
+++ Project/api/database/index.js
@@ -2,14 +2,16 @@
 const mysql = require('mysql2');
 const config = require('config');
 
-const HOST = config.database ? config.database.host : '';
-const USER = config.database ? config.database.user : '';
-const DATABASE = config.database ? config.database.database : '';
+const HOST = config.database ? config.database.host : 'localhost';
+const USER = config.database ? config.database.user : 'root';
+const DATABASE = config.database ? config.database.database : 'mysql';
+const PASSWORD = config.database ? config.database.password : '';
 // create the connection to database
 const connection = mysql.createConnection({
     host: HOST,
     user: USER,
-    database: DATABASE
+    database: DATABASE,
+    password: PASSWORD
 });
 
 module.exports = connection;
